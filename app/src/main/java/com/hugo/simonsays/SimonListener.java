f08f291d8ef2fdb2904a33e67ffc752c47aaf2d6
Hugo Zink <hugo.zink@hotmail.com>
2017-01-09 11:17:06 +0100

    Added Listener interface and Color enum.

---
+++ app/src/main/java/com/hugo/simonsays/SimonListener.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.hugo.simonsays;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Hugo
+ */
+public interface SimonListener
+{
+    void onGameStart();
+    void onGameStop();
+    void onGameTimeout();
+    
+    void onStartInput();
+    void onStopInput();
+    
+    void displayColors(ArrayList<Color> colors);
+}
