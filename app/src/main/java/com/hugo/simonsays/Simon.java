4c36bae99d4df28aabe6f2d34c047710ea3e5398
Danny <kenwai2010@gmail.com>
2017-01-09 11:18:24 +0100

    Add Simon Model

---
+++ app/src/main/java/com/hugo/simonsays/Simon.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simonsays;
+
+import java.util.*;
+
+/**
+ *
+ * @author Danny
+ */
+public class Simon
+{
+    private ArrayList<Color> colors;
+    
+    private Random random;
+    
+    public Simon()
+    {
+        random = new Random();
+        colors = new ArrayList<Color>();
+        
+        addRandomColor();
+    }
+    
+    public boolean checkColor(Color color, int index)
+    {
+        return colors.get(index) == color;
+    }
+    
+    public ArrayList<Color> getColors()
+    {
+        //Return a copy of the colors list, to prevent modification.
+        return new ArrayList<Color>(colors);
+    }
+    
+    public void addRandomColor()
+    {
+        colors.add(Color.getRandom());
+    }
+}
